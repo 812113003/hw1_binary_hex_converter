80da35af0f8a434cc95df7f4adce06e20714ed2b
812113003 <b812113003@tmu.edu.tw>
2025-03-14 15:26:37 +0800

    Update hw1_binary_hex_converter.py

--- hw1_binary_hex_converter.py
+++ hw1_binary_hex_converter.py
@@ -1,1 +1,18 @@
-print("aaa")
+while True:
+    try:
+        n = int(input())
+        bi = " "
+        a = n
+        while n > 0:
+            bi = str(n % 2) + bi
+            n //= 2
+        print(bi)
+        hex_digit = "0123456789ABCDEF"
+        h = ""
+        while a > 0:
+            r = a % 16
+            h = hex_digit[r] + h
+            a //= 16
+        print(h)
+    except EOFError:
+        break
